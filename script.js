1cd4135d2afc8580f4d6e655981797f1ccc33653
Ana Grbic <anagrbic0607@gmail.com>
2026-02-04 17:49:10 +0100

    zavrsen projekat

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Ana Grbić AI 2/2023, Đorđe Slavković AI 12/2023
 document.addEventListener("DOMContentLoaded", () => {
   // Funkcionalnost za dugmiće "Saznaj više"
   const buttons = document.querySelectorAll(".infoBtn");
@@ -13,27 +14,63 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (e) => {
       e.preventDefault(); // sprečava reload stranice
 
-      // Preuzimanje vrednosti iz polja
       const name = document.getElementById("name").value.trim();
       const email = document.getElementById("email").value.trim();
       const message = document.getElementById("message").value.trim();
 
-      // Osnovna validacija
       if (name === "" || email === "" || message === "") {
         alert("Molimo vas da popunite sva polja.");
         return;
       }
 
-      // Provera email formata (jednostavna)
       const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
       if (!email.match(emailPattern)) {
         alert("Unesite validnu email adresu.");
         return;
       }
 
-      // Ako je sve u redu
       alert("Hvala na poruci, " + name + "! Javićemo vam se uskoro.");
-      form.reset(); // resetuje polja forme
+      form.reset();
+    });
+  }
+
+  // Dugme "Na vrh"
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  if (scrollTopBtn) {
+    scrollTopBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  }
+
+  // Galerija – otvaranje uvećane slike u modalu
+  const modalImage = document.getElementById("modalImage");
+  const thumbs = document.querySelectorAll(".thumb");
+  if (modalImage && thumbs.length > 0) {
+    thumbs.forEach(img => {
+      img.addEventListener("click", () => {
+        const fullImg = img.getAttribute("data-img");
+        modalImage.src = fullImg;
+      });
     });
   }
 });
+
+// Deklarišemo konstantnu promenljivu 'citati' koja čuva niz (listu) tekstova
+const citati = [
+    '"Sreća prati hrabre."',
+    '"Znanje je moć."',
+    '"Bolje sprečiti nego lečiti."',
+    '"Ko rano rani, dve sreće grabi."'
+];
+
+
+// Definišemo funkciju pod nazivom 'noviCitat' koju možemo pozvati kasnije
+function noviCitat() {
+    // Generiše nasumičan broj na osnovu dužine niza
+    const randomIndex = Math.floor(Math.random() * citati.length);
+    //Npisemo u HTML tagu sa id-jem citat.
+    document.getElementById('citat').innerText = citati[randomIndex];
+}
